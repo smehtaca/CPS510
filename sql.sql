9b72351ab79b5f3ace6b4f5a28f7ac98c90003f4
Sameer Mehta <sameer-mehta@outlook.com>
2016-09-28 02:06:37 -0400

    Only 1 error this time, yay.

--- sql.sql
+++ sql.sql
@@ -19,7 +19,7 @@ employee_id int not null,
 manager_id int not null,
 store_number int not null,
 CONSTRAINT store_pk PRIMARY KEY(store_number),
-CONSTRAINT fk_employee FOREIGN KEY (employee_id,manager_id) REFERENCES employee(employee_id,manager_id)
+CONSTRAINT fk_employee_store FOREIGN KEY (employee_id,manager_id) REFERENCES employee(employee_id,manager_id)
 );
 
 CREATE TABLE product(
@@ -40,7 +40,7 @@ CONSTRAINT fk_store FOREIGN KEY (store_number) REFERENCES store(store_number)
 CREATE TABLE manufacturer_product(
 product_id int not null,
 manufacturer_id int not null,
-CONSTRAINT fk_product FOREIGN KEY(product_id) REFERENCES product(product_id),
+CONSTRAINT fk_product_manufacturer FOREIGN KEY(product_id) REFERENCES product(product_id),
 CONSTRAINT fk_manufacturer FOREIGN KEY(manufacturer_id) REFERENCES manufacturer(manufacturer_id),
 CONSTRAINT manufacturer_product__pk PRIMARY KEY(product_id,manufacturer_id)
 );
@@ -48,8 +48,9 @@ CONSTRAINT manufacturer_product__pk PRIMARY KEY(product_id,manufacturer_id)
 CREATE TABLE employee_product(
 product_id int not null,
 employee_id int not null,
-CONSTRAINT fk_product FOREIGN KEY(product_id) REFERENCES product(product_id),
-CONSTRAINT fk_employee FOREIGN KEY(employee_id) REFERENCES employee(employee_id),
+manufacturer_id not null,
+CONSTRAINT fk_product_employee FOREIGN KEY(product_id) REFERENCES product(product_id),
+CONSTRAINT fk_employee_product FOREIGN KEY(employee_id,manager_id) REFERENCES employee(employee_id,manager_id),
 CONSTRAINT employee_product_pk PRIMARY KEY(product_id,employee_id)
 );
 
@@ -59,7 +60,7 @@ lead_actor varchar(255),
 director varchar(255),
 production_company varchar(255) not null,
 CONSTRAINT movie_pk PRIMARY KEY(product_id),
-CONSTRAINT fk_prod FOREIGN KEY(product_id) REFERENCES product(product_id)
+CONSTRAINT fk_product_movie FOREIGN KEY(product_id) REFERENCES product(product_id)
 );
 
 CREATE TABLE music(
@@ -68,5 +69,5 @@ lead_artist varchar(255) not null,
 producer varchar(255) not null,
 record_label varchar(255),
 CONSTRAINT music_pk PRIMARY KEY(product_id),
-CONSTRAINT fk_productid FOREIGN KEY(product_id) REFERENCES product(product_id)
+CONSTRAINT fk_product_music FOREIGN KEY(product_id) REFERENCES product(product_id)
 );
